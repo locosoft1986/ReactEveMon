f86202f61883bd74ee99427b19b6dc013ee346be
Jeremy Weasley <locosoft1986@gmail.com>
2016-06-22 17:51:43 +0800

    fix Collapsible component  storybook layout problem

--- app/components/stories/ui.js
+++ app/components/stories/ui.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import {List} from 'react-toolbox';
-import Container from './container';
 import Collapsible from '../../components/UI/Collapsible';
 import CharList from '../../components/layouts/CharList';
 import {charListItems} from './mocks';
 
 storiesOf('UI', module)
   .add('Collapsible with List', () => (
-    <Container style={{position: 'relative', display: 'block'}}>
+    <div style={{position: 'relative', display: 'block'}}>
       <Collapsible triggerText='This is a Test A'>
         <CharList characters={charListItems} onSelect={action('selected A')} />
       </Collapsible>
@@ -21,5 +20,5 @@ storiesOf('UI', module)
       <Collapsible triggerText='This is a Test D'>
         <CharList characters={charListItems} onSelect={action('selected D')} />
       </Collapsible>
-    </Container>
+    </div>
   ));
